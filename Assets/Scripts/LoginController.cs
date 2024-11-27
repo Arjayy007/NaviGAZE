cdcdde67990c026376a52f1e14580293a1db987f
Arjayy007 <116645261+Arjayy007@users.noreply.github.com>
2024-11-27 19:57:31 +0800

    API Calls

--- Assets/Scripts/LoginController.cs
+++ Assets/Scripts/LoginController.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
+using PlayFab;
+using PlayFab.ClientModels;
 
-public class LoginController
+
+public class LoginController: MonoBehaviour
 {
-    
+  void Start()
+  {
+    Login();
+  }
+
+  void Login()
+  {
+    var request = new LoginWithCustomIDRequest { CustomId = "GettingStartedGuide", CreateAccount = true };
+    PlayFabClientAPI.LoginWithCustomID(request, OnLoginSuccess, OnLoginFailure);
+  }
+
+  void OnLoginSuccess(LoginResult result)
+  {
+    Debug.Log("Congratulations, you made your first successful API call!");
+  }
+
+  void OnLoginFailure(PlayFabError error)
+  {
+    Debug.LogWarning("Something went wrong with your first API call.  :(");
+    Debug.LogError("Here's some debug information:");
+    Debug.LogError(error.GenerateErrorReport());
+  }
+
 }
